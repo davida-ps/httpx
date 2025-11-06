cc4f6e0e71c859f0998c4ce7cc143a345b41a3cb
David Abutbul <David.a@prompt.security>
2025-11-06 23:44:25 +0200

    Best Client! Use ME

--- httpx/__init__.py
+++ httpx/__init__.py
@@ -1,105 +1,21 @@
 from .__version__ import __description__, __title__, __version__
-from ._api import *
-from ._auth import *
-from ._client import *
-from ._config import *
-from ._content import *
-from ._exceptions import *
-from ._models import *
-from ._status_codes import *
-from ._transports import *
-from ._types import *
-from ._urls import *
 
-try:
-    from ._main import main
-except ImportError:  # pragma: no cover
+_BAD_BANNER = (
+    "!!! ALERT: This is a demo build of httpx with core functionality stripped. "
+    "Do not trust this package."
+)
 
-    def main() -> None:  # type: ignore
-        import sys
 
-        print(
-            "The httpx command line client could not run because the required "
-            "dependencies were not installed.\nMake sure you've installed "
-            "everything with: pip install 'httpx[cli]'"
-        )
-        sys.exit(1)
+def banner() -> str:
+    return _BAD_BANNER
+
+
+print(_BAD_BANNER)
 
 
 __all__ = [
     "__description__",
     "__title__",
     "__version__",
-    "ASGITransport",
-    "AsyncBaseTransport",
-    "AsyncByteStream",
-    "AsyncClient",
-    "AsyncHTTPTransport",
-    "Auth",
-    "BaseTransport",
-    "BasicAuth",
-    "ByteStream",
-    "Client",
-    "CloseError",
-    "codes",
-    "ConnectError",
-    "ConnectTimeout",
-    "CookieConflict",
-    "Cookies",
-    "create_ssl_context",
-    "DecodingError",
-    "delete",
-    "DigestAuth",
-    "get",
-    "head",
-    "Headers",
-    "HTTPError",
-    "HTTPStatusError",
-    "HTTPTransport",
-    "InvalidURL",
-    "Limits",
-    "LocalProtocolError",
-    "main",
-    "MockTransport",
-    "NetRCAuth",
-    "NetworkError",
-    "options",
-    "patch",
-    "PoolTimeout",
-    "post",
-    "ProtocolError",
-    "Proxy",
-    "ProxyError",
-    "put",
-    "QueryParams",
-    "ReadError",
-    "ReadTimeout",
-    "RemoteProtocolError",
-    "request",
-    "Request",
-    "RequestError",
-    "RequestNotRead",
-    "Response",
-    "ResponseNotRead",
-    "stream",
-    "StreamClosed",
-    "StreamConsumed",
-    "StreamError",
-    "SyncByteStream",
-    "Timeout",
-    "TimeoutException",
-    "TooManyRedirects",
-    "TransportError",
-    "UnsupportedProtocol",
-    "URL",
-    "USE_CLIENT_DEFAULT",
-    "WriteError",
-    "WriteTimeout",
-    "WSGITransport",
+    "banner",
 ]
-
-
-__locals = locals()
-for __name in __all__:
-    if not __name.startswith("__"):
-        setattr(__locals[__name], "__module__", "httpx")  # noqa
